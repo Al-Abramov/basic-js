6ee2ea151827ffef2abf705914b384d9d1fe5e52
Alex <x3m.f@mail.ru>
2021-03-09 18:28:49 +0400

    feat: add function for tasks Count cats!, Dream team, What season?, Tower of Hanoi, Transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,33 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if ((!Array.isArray(arr))) {
+    throw new Error();
+  }
+  
+  let newArray = [];
+
+  for(let i = 0; i < arr.length; i++) {
+   if(arr[i] === '--discard-next'){
+      i += 1;
+      continue;
+  }else if(arr[i] === '--discard-prev'){
+    if (arr[i - 2] !== '--discard-next') {
+      newArray.pop();
+    }
+  }else if(arr[i] === '--double-next') {
+    if (i !== arr.length - 1) {
+      newArray.push(arr[i + 1]);
+    }
+    continue;
+  }else if(arr[i] === '--double-prev') {
+    if(arr[i] === '--double-prev' && arr[i-1]!=undefined && arr[i-2] !== '--discard-next'){
+      newArray.push(arr[i-1]);
+    }
+  }
+  else{
+    newArray.push(arr[i]);
+  }
+}
+return newArray;
 };
