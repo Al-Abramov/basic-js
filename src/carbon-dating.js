540250ea283c70165bc99ccd159d5dd35426e520
Alex <x3m.f@mail.ru>
2021-03-11 16:02:33 +0400

    feat: fixed function for tasks Carbon dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -4,12 +4,13 @@ const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(sampleActivity) {
-  if(typeof sampleActivity != String) {
-      return false;
-  }
+  if(typeof sampleActivity != String ||
+typeof sampleActivity == undefined) {
+    return false;
+}
 
 
-    let t = (parseInt(sampleActivity) * HALF_LIFE_PERIOD) / MODERN_ACTIVITY;
+  let t = (parseInt(sampleActivity) * HALF_LIFE_PERIOD) / MODERN_ACTIVITY;
 
-   return Math.ceil(t); 
+ return Math.ceil(t); 
 };
