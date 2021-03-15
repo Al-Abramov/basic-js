baf36e6c4b15219713de9cf889bfabfb0db53706
Alex <x3m.f@mail.ru>
2021-03-15 16:38:30 +0400

    feat: add function for tasks extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,32 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  if(typeof options.repeatTimes === 'undefined'){
+    options.repeatTimes = 1;
+  }
+  if(typeof options.separator === 'undefined') {
+    options.separator = '+';
+  }
+  if(typeof options.addition === 'undefined') {
+    options.addition = '';
+  }
+  if(typeof options.additionRepeatTimes === 'undefined') {
+    options.additionRepeatTimes = 1;
+  }
+  if(typeof options.additionSeparator === 'undefined') {
+    options.additionSeparator = '|';
+  }
+
+  let newStr = '';
+  for(let i = 0; i < options.repeatTimes; i++) {
+    if(i === options.repeatTimes-1){
+    newStr += str + (options.addition + options.additionSeparator)
+          .repeat(options.additionRepeatTimes-1) + options.addition;
+    }else {
+    newStr += str + (options.addition + options.additionSeparator)
+          .repeat(options.additionRepeatTimes-1) + options.addition + options.separator;
+    }
+  }
+  return newStr;
 };
   
